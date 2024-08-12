ab8ef543e2dcb8bdc1784d031ca6d2ec7e56d740
DaveMagbag <dmagbag2023@plm.edu.ph>
2024-08-12 22:54:15 +0800

    files

---
+++ script.js
@@ -0,0 +1,26 @@
+const confettiContainer = document.getElementById('confetti-container');
+const videoContainer = document.getElementById('video-container');
+
+function createConfetti() {
+    const confetti = document.createElement('div');
+    confetti.classList.add('confetti');
+    
+    confetti.style.left = Math.random() * 100 + 'vw';
+    confetti.style.top = Math.random() * -100 + 'px'; // Start above the viewport
+    confetti.style.animationDuration = Math.random() * 3 + 2 + 's';
+    confetti.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+
+    confettiContainer.appendChild(confetti);
+
+    setTimeout(() => {
+        confetti.remove();
+    }, 5000);
+}
+
+setInterval(createConfetti, 300);
+
+function showVideo() {
+    document.querySelector('.click-box').style.display = 'none';
+    videoContainer.style.display = 'block';
+}
+
